094bffd9d981a7b08f7bfc2fc67b4c51cb289999
Ash <acst52@gmail.com>
2023-02-02 17:12:00 -0500

    Started working on generateMd.js fcns

--- develop/utils/generateMarkdown.js
+++ develop/utils/generateMarkdown.js
@@ -4,7 +4,11 @@ function renderLicenseBadge(license) {}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license !== 'none') {
+    return `* [License](#license)` // TOC link
+  } return "";
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
